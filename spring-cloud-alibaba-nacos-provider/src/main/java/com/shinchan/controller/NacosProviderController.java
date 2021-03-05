22c9fef23184a928b5f474fb27d5c161a91a6449
wtt <taotao.wang1@pactera.com>
2021-03-05 14:47:35 +0800

    修改编码规范

--- spring-cloud-alibaba-nacos-provider/src/main/java/com/shinchan/controller/NacosProviderController.java
+++ spring-cloud-alibaba-nacos-provider/src/main/java/com/shinchan/controller/NacosProviderController.java
@@ -1,34 +1,36 @@
 package com.shinchan.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
+/**
+ * @author Administrator
+ */
 @RestController
 public class NacosProviderController {
 
     @Value("${server.port}")
     private String port;
 
-    // 注入配置文件上下文
-    @Autowired
+    /**
+     * 注入配置文件上下文
+     */
+    @Resource
     private ConfigurableApplicationContext applicationContext;
-
-    /*@GetMapping(value = "/test/{message}")
-    public String test(@PathVariable String message) {
-        return "Hello Nacos Discovery " + message + " i am from port " + port;
-    }*/
 
     @GetMapping(value = "/test/{message}")
     public String test(@PathVariable String message) {
         return "Hello Nacos Discovery " + message + " i am from port " + port;
     }
 
-    // 从上下文中读取配置
+    /**
+     * 从上下文中读取配置
+     */
     @GetMapping(value = "/hi")
     public String sayHi() {
         return "Hello " + applicationContext.getEnvironment().getProperty("user.name");
